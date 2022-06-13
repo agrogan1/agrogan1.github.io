505d0e4d89afce7754db791c100d14a678a0fd91
Grogan-Kaylor <agrogan@UMICH.EDU>
2022-06-13 10:47:47 -0400

    first commit

---
+++ poetry/buildPDFs.R
@@ -0,0 +1,10 @@
+# build poetry slides to PDF
+
+library(xaringanBuilder)
+
+build_pdf("./poetry/transcendental-etude.Rmd")
+
+build_pdf("./poetry/why-cant-numbers-be-beautiful-too.Rmd")
+
+build_pdf("./poetry/its-not-just-what-youre-born-with.Rmd")
+
