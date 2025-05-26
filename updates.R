16de9544a9e6f8aa9c0e3f680c9b4629bf38dfa8
Andrew Grogan-Kaylor <agrogan@m-m0vy37d2td.local>
2025-05-26 15:50:16 -0400

    fix accents in Spanish names

--- updates.R
+++ updates.R
@@ -8,3 +8,20 @@ mybib <- sort(mybib, sorting = "nyt") # sort by name - year - title
 
 WriteBib(mybib, file = "agrogan.bib")
 
+# fix strings with Spanish accents
+# because RefManageR somehow kludges them
+
+bibliography  <- readLines("agrogan.bib")
+
+bibliography  <- gsub(pattern = "Ignacio Mart{\\a'\\i}n-Bar{\\a'o}", 
+                      replace = "Ignacio Martín-Baró", 
+                      x = bibliography,
+                      fixed = TRUE)
+
+bibliography  <- gsub(pattern = "Ellacur{\\a'\\i}a", 
+                      replace = "Ellacuría", 
+                      x = bibliography,
+                      fixed = TRUE)
+
+
+writeLines(bibliography, con="agrogan.bib")
